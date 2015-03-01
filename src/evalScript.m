840e4d79d7491e604606f987a9771a7655a5a381
JSB <jakobworldpeace@bluewin.ch>
2015-02-28 21:10:41 -0500

    Display metrics

--- src/evalScript.m
+++ src/evalScript.m
@@ -24,7 +24,12 @@ load(pathToGround);
 % load(pathToOutput);
 % TODO this is a workaround to test evalScript
 % output = circshift(roundedEventArray, 0);
-output = circshift(roundedEventArray, 2);
+output= roundedEventArray;
+output(:,1,:,:) = circshift(output(:,1,:,:), 0);
+output(:,2,:,:) = circshift(output(:,2,:,:), 1);
+output(:,3,:,:) = circshift(output(:,3,:,:), 2);
+output(:,4,:,:) = circshift(output(:,4,:,:), 3);
+% output(find(output(:,1,1,1),1),1,1,1) = 0;
 if ~isequal(size(roundedEventArray), size(output))
     msgID = 'MATLAB:inputError';
     msg = 'Input size differs from ground truth.';
@@ -39,7 +44,7 @@ end
 [p q r s] = ind2sub(size(output),find(output));
 
 %
-% Calculate stats
+% Aggregate Metrics: L1/L2/0-1 Loss
 %
 metrics = zeros(2,nEventTypes,nSubjects,nTrials);
 for i = 1:nEventTypes
@@ -47,14 +52,25 @@ for i = 1:nEventTypes
         for k = 1:nTrials
             y = l((m==i)&(n==j)&(o==k));
             yhat = p((q==i)&(r==j)&(s==k));
-            assert(isequal(size(y),size(yhat)));
-            % TODO what if not the same number of events?
-            metrics(1,i,j,k) = norm(y-yhat,1); % L1 loss
-            metrics(2,i,j,k) = norm(y-yhat,2); % L2 loss
-            metrics(3,i,j,k) = sum(y~=yhat); % 0-1 loss
+            % assert(isequal(size(y),size(yhat)));
+            if isequal(size(y),size(yhat))
+                metrics(1,i,j,k) = norm(y-yhat,1)/size(y,1); % L1 loss
+                metrics(2,i,j,k) = norm(y-yhat,2)/size(y,1); % L2 loss
+                metrics(3,i,j,k) = sum(y~=yhat)/size(y,1); % 0-1 loss
+            else
+                metrics(1,i,j,k) = NaN;
+                metrics(2,i,j,k) = NaN;
+                metrics(3,i,j,k) = NaN;
+            end
         end
     end
 end
+
+display('Aggregate L1/L2/0-1 Loss for LHS RTO RHS LTO')
+display(mean(mean(metrics,4),3))
+
+display('Per subject/trial L1/L2/0-1 Loss for LHS RTO RHS LTO')
+display(metrics)
 
 %
 % Pad array
