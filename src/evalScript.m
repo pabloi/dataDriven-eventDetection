d66af6bc16d03c22e70faf2fc90ec4501ebcbfc2
JSB <jakobworldpeace@bluewin.ch>
2015-03-01 21:50:07 -0500

    Add basic per cycle metrics

--- src/evalScript.m
+++ src/evalScript.m
@@ -29,7 +29,7 @@ output(:,1,:,:) = circshift(output(:,1,:,:), 0);
 output(:,2,:,:) = circshift(output(:,2,:,:), 1);
 output(:,3,:,:) = circshift(output(:,3,:,:), 2);
 output(:,4,:,:) = circshift(output(:,4,:,:), 3);
-% output(find(output(:,1,1,1),1),1,1,1) = 0;
+output(find(output(:,1,1,1),1),1,1,1) = 0;
 if ~isequal(size(roundedEventArray), size(output))
     msgID = 'MATLAB:inputError';
     msg = 'Input size differs from ground truth.';
@@ -44,55 +44,63 @@ end
 [p q r s] = ind2sub(size(output),find(output));
 
 %
-% Aggregate Metrics: L1/L2/0-1 Loss
+% Compute aggregate metrics: L1/L2/0-1 loss
 %
-metrics = zeros(2,nEventTypes,nSubjects,nTrials);
+loss = zeros(3,nEventTypes,nSubjects,nTrials);
 for i = 1:nEventTypes
     for j = 1:nSubjects
         for k = 1:nTrials
             y = l((m==i)&(n==j)&(o==k));
             yhat = p((q==i)&(r==j)&(s==k));
-            % assert(isequal(size(y),size(yhat)));
             if isequal(size(y),size(yhat))
-                metrics(1,i,j,k) = norm(y-yhat,1)/size(y,1); % L1 loss
-                metrics(2,i,j,k) = norm(y-yhat,2)/size(y,1); % L2 loss
-                metrics(3,i,j,k) = sum(y~=yhat)/size(y,1); % 0-1 loss
+                loss(1,i,j,k) = norm(y-yhat,1)/length(y); % L1 loss
+                loss(2,i,j,k) = norm(y-yhat,2)/length(y); % L2 loss
+                loss(3,i,j,k) = sum(y~=yhat)/length(y); % 0-1 loss
             else
-                metrics(1,i,j,k) = NaN;
-                metrics(2,i,j,k) = NaN;
-                metrics(3,i,j,k) = NaN;
+                loss(1,i,j,k) = NaN;
+                loss(2,i,j,k) = NaN;
+                loss(3,i,j,k) = NaN;
             end
         end
     end
 end
 
-display('Aggregate L1/L2/0-1 Loss for LHS RTO RHS LTO')
-display(mean(mean(metrics,4),3))
+display('Average L1/L2/0-1 Loss for LHS RTO RHS LTO')
+display(mean(mean(loss,4),3))
 
 display('Per subject/trial L1/L2/0-1 Loss for LHS RTO RHS LTO')
+display(loss)
+
+%
+% Compute per cycle metrics: tp fp tn fn
+%
+hn = 0.5*ones(1,2);
+metrics = zeros(3,nEventTypes,nSubjects,nTrials);
+for i = 1:nEventTypes
+    for j = 1:nSubjects
+        for k = 1:nTrials
+            y = l((m==i)&(n==j)&(o==k));
+            y = [1 y' nSamples]';
+            yhat = p((q==i)&(r==j)&(s==k));
+            yCycles = round(conv(y,hn,'valid'));
+            for t = 1:length(yCycles)-1
+                nbhat = sum(output(yCycles(t):yCycles(t+1),i,j,k));
+                if nbhat > 0
+                    metrics(1,i,j,k) = metrics(1,i,j,k) + 1;
+                    metrics(2,i,j,k) = metrics(2,i,j,k) + (nbhat - 1);
+                    metrics(3,i,j,k) = metrics(3,i,j,k) + 0;
+                else
+                    metrics(1,i,j,k) = metrics(1,i,j,k) + 0;
+                    metrics(2,i,j,k) = metrics(2,i,j,k) + 0;
+                    metrics(3,i,j,k) = metrics(3,i,j,k) + 1;
+                end
+            end
+        end
+    end
+end
+
+% display('TP FP FN for LHS RTO RHS LTO')
+% display(mean(mean(metrics,4),3))
+
+display('Per subject/trial TP FP FN for LHS RTO RHS LTO')
 display(metrics)
-
-%
-% Pad array
-%
-% padsize = 100;
-% outputPadded = padarray(output,padsize);
-% outputPadded(1,:,:,:) = 1;
-% outputPadded(end,:,:,:) = 1;
-% roundedPadded = padarray(roundedEventArray,padsize);
-% roundedPadded(1,:,:,:) = 1;
-% roundedPadded(end,:,:,:) = 1;
-
-%% TODO window method
-% hn = 0.5*ones(1,2);
-% for i = 1:nEventTypes
-%     for j = 1:nSubjects
-%         for k = 1:nTrials
-%             xn = l((m==i)&(n==j)&(o==k));
-%             ixCycles = round(conv(xn,hn,'same'));
-%             for p = 1:size(ixCycles)-2
-%                 assert(sum(outputPadded(ixCycles(p):ixCycles(p+1),i,j,k))==1)
-%             end
-%         end
-%     end
-% end
