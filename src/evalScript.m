93767cde94cb87d04345015d6d93e1ccb07ad144
JSB <jakobworldpeace@bluewin.ch>
2015-03-08 12:10:15 -0400

    Add boxplots

--- src/evalScript.m
+++ src/evalScript.m
@@ -32,7 +32,7 @@ trueErrors = computeTrueErrors(roundedEventArray, output);
 
 % ------------------------------------------------------------------------------
 
-% Plot true errors per event type
+% Histogram of true errors per event type
 if plotting
     figure(1)
 else
@@ -41,7 +41,11 @@ end
 for i = 1:nEventTypes
     subplot(2,2,i)
     TE = cat(1, trueErrors{i,:,:});
-    missing = any(isnan(TE));
+    if any(isnan(TE))
+        missing = 'yes';
+    else
+        missing = 'no';
+    end
     maxval = max(abs(TE));
     hist(TE,[-(maxval+1):maxval+1]);
     title(['Event Type ',int2str(i)])
@@ -49,7 +53,7 @@ for i = 1:nEventTypes
         'True Error (frames) (n=',...
         int2str(length(TE)),...
         ', na=',...
-        int2str(missing),...
+        missing,...
         ')'...
         ])
     ylabel('Frequency')
@@ -58,7 +62,7 @@ if saveplot
     saveas(gcf, strcat(figpath,'trueErrorsPerEventType'), 'png')
 end
 
-% Plot true errors per event type and subject
+% Histogram of true errors per event type and subject
 if plotting
     figure(2)
 else
@@ -69,7 +73,11 @@ for i = 1:nEventTypes
         % subplot(nSubjects,nEventTypes,nSubjects*(i-1)+j)
         subplot(nSubjects,nEventTypes,nEventTypes*(j-1)+i)
         TE = cat(1, trueErrors{i,j,:});
-        missing = any(isnan(TE));
+        if any(isnan(TE))
+            missing = 'yes';
+        else
+            missing = 'no';
+        end
         maxval = max(abs(TE));
         hist(TE,[-(maxval+1):maxval+1]);
         title(['Subject ',int2str(j),' Event Type ',int2str(i)])
@@ -77,7 +85,7 @@ for i = 1:nEventTypes
             'True Error (frames) (n=',...
             int2str(length(TE)),...
             ', na=',...
-            int2str(missing),...
+            missing,...
             ')'...
             ])
         ylabel('Frequency')
@@ -86,3 +94,159 @@ end
 if saveplot
     saveas(gcf, strcat(figpath,'trueErrorsPerEventTypeAndSubject'), 'png')
 end
+
+% ------------------------------------------------------------------------------
+
+% Histogram of absolute errors per event type
+if plotting
+    figure(3)
+else
+    figure('visible','off')
+end
+for i = 1:nEventTypes
+    subplot(2,2,i)
+    AE = abs(cat(1, trueErrors{i,:,:}));
+    if any(isnan(TE))
+        missing = 'yes';
+    else
+        missing = 'no';
+    end
+    maxval = max(AE);
+    hist(AE,[0:maxval+1]);
+    title(['Event Type ',int2str(i)])
+    xlabel([...
+        'Absolute Error (frames) (n=',...
+        int2str(length(AE)),...
+        ', na=',...
+        missing,...
+        ')'...
+        ])
+    ylabel('Frequency')
+end
+if saveplot
+    saveas(gcf, strcat(figpath,'absoluteErrorsPerEventType'), 'png')
+end
+
+% Histogram absolute errors per event type and subject
+if plotting
+    figure(4)
+else
+    figure('visible','off')
+end
+for i = 1:nEventTypes
+    for j = 1:nSubjects
+        % subplot(nSubjects,nEventTypes,nSubjects*(i-1)+j)
+        subplot(nSubjects,nEventTypes,nEventTypes*(j-1)+i)
+        AE = abs(cat(1, trueErrors{i,j,:}));
+        if any(isnan(TE))
+            missing = 'yes';
+        else
+            missing = 'no';
+        end
+        maxval = max(AE);
+        hist(AE,[0:maxval+1]);
+        title(['Subject ',int2str(j),' Event Type ',int2str(i)])
+        xlabel([...
+            'Absolute Error (frames) (n=',...
+            int2str(length(AE)),...
+            ', na=',...
+            missing,...
+            ')'...
+            ])
+        ylabel('Frequency')
+    end
+end
+if saveplot
+    saveas(gcf, strcat(figpath,'absoluteErrorsPerEventTypeAndSubject'), 'png')
+end
+
+% ------------------------------------------------------------------------------
+
+% Boxplot of true errors per event type
+if plotting
+    figure(5)
+else
+    figure('visible','off')
+end
+TE = cat(1, trueErrors{:,:,:});
+groupsize = [];
+for i = 1:nEventTypes
+    groupsize = [groupsize i*ones(1,length(cat(1, trueErrors{i,:,:})))];
+end
+boxplot(TE,groupsize)
+title('True Error per Event Type')
+xlabel('Event Type')
+ylabel('True Error (frames)')
+if saveplot
+    saveas(gcf, strcat(figpath,'trueErrorsBoxplot'), 'png')
+end
+
+% Boxplot of true errors per event and subject
+if plotting
+    figure(6)
+else
+    figure('visible','off')
+end
+for i = 1:nEventTypes
+    subplot(nEventTypes,1,i)
+    TE = cat(1, trueErrors{i,:,:});
+    groupsize = [];
+    for k = 1:nSubjects
+        groupsize = [groupsize k*ones(1,length(cat(1, trueErrors{i,k,:})))];
+    end
+    boxplot(TE,groupsize)
+    title(['Event Type ',int2str(i)])
+    xlabel('Subject')
+    ylabel('True Error (frames)')
+end
+if saveplot
+    saveas(gcf, strcat(figpath,'trueErrorsPerEventTypeBoxplot'), 'png')
+end
+
+% Boxplot of true errors per subject
+if plotting
+    figure(7)
+else
+    figure('visible','off')
+end
+for i = 1:nSubjects
+    subplot(nSubjects,1,i)
+    TE = cat(1, trueErrors{:,i,:});
+    groupsize = [];
+    for k = 1:nEventTypes
+        groupsize = [groupsize k*ones(1,length(cat(1, trueErrors{k,i,:})))];
+    end
+    boxplot(TE,groupsize)
+    title(['Subject ',int2str(i)])
+    xlabel('Event Type')
+    ylabel('True Error (frames)')
+end
+if saveplot
+    saveas(gcf, strcat(figpath,'trueErrorsPerSubjectBoxplot'), 'png')
+end
+% Boxplot true errors per event type and subject
+if plotting
+    figure(8)
+else
+    figure('visible','off')
+end
+for i = 1:nEventTypes
+    for j = 1:nSubjects
+        % subplot(nSubjects,nEventTypes,nSubjects*(i-1)+j)
+        subplot(nSubjects,nEventTypes,nEventTypes*(j-1)+i)
+        TE = cat(1, trueErrors{i,j,:});
+        if any(isnan(TE))
+            missing = 'yes';
+        else
+            missing = 'no';
+        end
+        boxplot(TE)
+        title(['Subject ',int2str(j),' Event Type ',int2str(i)])
+        ylabel('TE (frames)')
+    end
+end
+if saveplot
+    saveas(gcf, strcat(figpath,'trueErrorsPerEventTypeAndSubjectBoxplot'), 'png')
+end
+
+% ------------------------------------------------------------------------------
