b2be2a69aa54be5b0595822a783ffb165a1f34bc
summivox <summivox@gmail.com>
2015-03-22 00:59:16 -0400

    test (it still doesn't work)

--- lstm/lstm_naive.m
+++ lstm/lstm_naive.m
@@ -1,35 +1,35 @@
-function nn = lstm_naive()
+function net = lstm_naive()
 
-nn = network;
-nn.name = 'LSTM naive';
+net = network;
+net.name = 'LSTM naive';
 
 
 % boundary
 
-nn.numInputs = 1;  % x ; h', c'
-nn.numLayers = 8;  % i, f, o, z, iz, fc', h, c
-nn.outputConnect = [0 0 0 0 0 0 1 0]; % h, c
+net.numInputs = 1;  % x ; h', c'
+net.numLayers = 8;  % i, f, o, z, iz, fc', h, c
+net.outputConnect = [0 0 0 0 0 0 1 0]; % h, c
 
-nn.outputs{7}.name = 'h';
-nn.outputs{7}.feedbackDelay = 1;
-nn.outputs{7}.feedbackInput = 2;
-nn.outputs{7}.feedbackMode = 'open';
+net.outputs{7}.name = 'h';
+net.outputs{7}.feedbackDelay = 1;
+net.outputs{7}.feedbackInput = 2;
+net.outputs{7}.feedbackMode = 'open';
 
 % nn.outputs{8}.name = 'c';
 % nn.outputs{8}.feedbackDelay = 1;
 % nn.outputs{8}.feedbackInput = 3;
 % nn.outputs{8}.feedbackMode = 'open';
 
-nn.inputs{1}.name = 'x';
-nn.inputs{2}.name = 'h_';
+net.inputs{1}.name = 'x';
+net.inputs{2}.name = 'h_';
 % nn.inputs{3}.name = 'c_';
 
 
 % connection
 
-nn.biasConnect = [1 1 1 1 0 0 0 0].'; % i, f, o, z
+net.biasConnect = [1 1 1 1 0 0 0 0].'; % i, f, o, z
 
-nn.inputConnect = [
+net.inputConnect = [
     1 1 1 1 0 0 0 0 % x  -> i, f, o, z
     1 1 1 1 0 0 0 0 % h' -> i, f, o, z
 ].';
@@ -42,11 +42,11 @@ LC(4, 5) = 1; % z   -> iz
 LC(5, 8) = 1; % iz  -> c
 LC(6, 8) = 1; % fc' -> c
 LC(8, :) = [1 1 1 0 0 1 1 0]; % c' -> i, f, o, fc', h (internal feedback)
-nn.layerConnect = LC.';
+net.layerConnect = LC.';
 
 % setup internal feedback delay
 for i = find(LC(8, :))
-    nn.layerWeights{i, 8}.delays = 1;
+    net.layerWeights{i, 8}.delays = 1;
 end
 
 
@@ -63,15 +63,16 @@ L = {
     'c'  , 'netsum' , 'purelin'
 };
 for i = 1:8
-    nn.layers{i}.name = L{i, 1};
-    nn.layers{i}.netInputFcn = L{i, 2};
-    nn.layers{i}.transferFcn = L{i, 3};
-    nn.layers{i}.size = 1;
+    net.layers{i}.name = L{i, 1};
+    net.layers{i}.netInputFcn = L{i, 2};
+    net.layers{i}.transferFcn = L{i, 3};
+    net.layers{i}.size = 1;
 end
 
 % nn = closeloop(nn);
-nn.trainFcn = 'trainlm';
-nn.divideFcn = 'dividerand';
-nn.divideParam.trainRatio = 70/100;
-nn.divideParam.valRatio = 15/100;
-nn.divideParam.testRatio = 15/100;
+net.trainFcn = 'trainlm';
+net.divideFcn = 'dividerand';
+net.divideMode = 'value';
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
--- lstm/lstm_test.m
+++ lstm/lstm_test.m
@@ -1,19 +1,30 @@
 %% load data
-load('dataArrays.mat')
+% load('dataArrays.mat')
 
 %% take subject 1
 mo = squeeze(motionArray(:, :, 1, :));
 ev = squeeze(roundedEventArray(:, :, 1, :));
 [mo_ts, ev_ts] = lstm_reshape(mo, ev, eventsToBeUsed);
 
+%% testing
+mo2 = squeeze(motionArray(:, :, 1, 3));
+ev2 = squeeze(roundedEventArray(:, :, 1, 3));
+[mo_ts2, ev_ts2] = lstm_reshape(mo2, ev2, eventsToBeUsed);
+
 %% prepare network and time series
-delay = 20;
+delay = 0;
+% delay = 10;
 
 nn = lstm_naive;
-nnd = adddelay(lstm_naive, delay);
 
 [Xs,Xi,Ai,Ts] = preparets(nnd, mo_ts, {}, ev_ts);
-Ts = {Ts{1:end-delay+1}};
+
+if delay > 0
+    nnd = adddelay(lstm_naive, delay);
+    Ts = {Ts{1:end-delay+1}};
+else
+    nnd = nn;
+end
 
 %% train
 nndt = train(nnd, Xs, Ts, Xi, Ai); % FIXME
