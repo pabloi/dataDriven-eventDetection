72fbbf26109927dfe325a396055f4ef8e706070a
JSB <jakobworldpeace@bluewin.ch>
2015-04-11 22:44:08 -0400

    Shorten sequence length to 100 samples

--- practical6/ed_train.lua
+++ practical6/ed_train.lua
@@ -99,8 +99,8 @@ function feval(x)
     -- local y = y11
     local x = X11:t()
     local y = y11:t()
-    print('x:size()' ..x:size())
-    print('y:size()' ..y:size())
+    print('x:size()' ..x:size(1) ..',' ..x:size(2))
+    print('y:size()' ..y:size(1) ..',' ..y:size(2))
 
     ------------------- forward pass -------------------
     local embeddings = {}            -- input embeddings
