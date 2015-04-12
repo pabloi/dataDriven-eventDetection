44c3d0e2ed9d0c2566b18cf0af6352a1df569616
JSB <jakobworldpeace@bluewin.ch>
2015-04-11 22:40:02 -0400

    Shorten sequence length to 100 samples

--- practical6/ed_train.lua
+++ practical6/ed_train.lua
@@ -47,8 +47,10 @@ print('loading data files...')
 local myFile = hdf5.open('data.h5')
 local data = myFile:all()
 -- TODO Use this to debug
-X11 = data['1']['1']['X'][{{1,opt.seq_length}, {}}]
-y11 = data['1']['1']['y'][{{1,opt.seq_length}, {}}]
+X11 = data['1']['1']['X']
+y11 = data['1']['1']['y']
+-- X11 = data['1']['1']['X'][{{1,opt.seq_length}, {}}]
+-- y11 = data['1']['1']['y'][{{1,opt.seq_length}, {}}]
 print('X11:size()' ..X11:size())
 print('y11:size()' ..y11:size())
 local vocab_size = 4
