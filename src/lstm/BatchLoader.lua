bfdb5189dc50dc7c9d020034e80926666f9f21a4
pabloi <itu.pablo@gmail.com>
2015-04-21 20:20:35 -0400

    ""

---
+++ src/lstm/BatchLoader.lua
@@ -0,0 +1,73 @@
+#!/usr/bin/env lua
+-- loader for character-level language models
+
+require 'torch'
+require 'math'
+require 'hdf5'
+
+local BatchLoader = {}
+BatchLoader.__index = BatchLoader
+
+function BatchLoader.create(data_file, batch_size, seq_length)
+    local self = {}
+    setmetatable(self, BatchLoader)
+
+    -- construct a tensor with the data for the subject and trial
+    print('loading data files...')
+    local myFile = hdf5.open(data_file)
+    local data = myFile:all()
+
+    -- self.batches is a table of tensors
+    self.batch_size = batch_size
+    self.seq_length = seq_length
+
+    self.x_batches = {}
+    self.y_batches = {}
+    self.nbatches = 0
+
+    -- ipairs does not work because the indices are strings ('1') and not integers
+    for subjects, trials in pairs(data) do
+        for trial, samples in pairs(trials) do
+            local xData = samples['X']
+            local yData = samples['y']
+
+            -- truncate data to multiple of batch_size * seq_length
+            local len = xData:size(1)
+            if len % (batch_size * seq_length) ~= 0 then
+                xData = xData:sub(1, batch_size * seq_length
+                            * math.floor(len / (batch_size * seq_length)))
+                yData = yData:sub(1, batch_size * seq_length
+                            * math.floor(len / (batch_size * seq_length)))
+            end
+
+            local new_x_batches = xData:split(batch_size*seq_length, 1)
+            local new_y_batches = yData:split(batch_size*seq_length, 1)
+            local new_nbatches = #new_x_batches
+            for k, v in ipairs(new_x_batches) do
+                self.x_batches[#self.x_batches + 1] = v
+            end
+            for k, v in ipairs(new_y_batches) do
+                self.y_batches[#self.y_batches + 1] = v
+            end
+            self.nbatches = self.nbatches + new_nbatches
+        end
+    end
+    assert(#self.x_batches == #self.y_batches)
+
+    self.current_batch = 0
+    self.evaluated_batches = 0  -- number of times next_batch() called
+
+    print('data load done.')
+    collectgarbage()
+    return self
+end
+
+function BatchLoader:next_batch()
+    self.current_batch = (self.current_batch % self.nbatches) + 1
+    self.evaluated_batches = self.evaluated_batches + 1
+    -- TODO transpose?
+    return self.x_batches[self.current_batch]:t(),
+        self.y_batches[self.current_batch]:t()
+end
+
+return BatchLoader
---
+++ src/lstm/test.lua
@@ -0,0 +1,65 @@
+require 'torch'
+require 'nn'
+require 'nngraph'
+require 'optim'
+require 'hdf5'
+
+-- local BatchLoader = require 'data.BatchLoader'
+local BatchLoader = require 'BatchLoader'
+local LSTM = require 'LSTM'             -- LSTM timestep and utilities
+require 'Embedding'                     -- class name is Embedding (not namespaced)
+local model_utils=require 'model_utils'
+
+
+cmd:text()
+cmd:text('Test a simple character-level LSTM language model')
+cmd:text()
+cmd:text('Options')
+cmd:option('-model','results.t7','contains just the protos table, and nothing else')
+cmd:option('-seed',123,'random number generator\'s seed')
+cmd:option('-sample',false,'false to use max at each timestep, true to sample at each timestep')
+cmd:option('-length',200,'number of characters to sample')
+cmd:text()
+
+-- parse input params
+opt = cmd:parse(arg)
+
+-- preparation stuff:
+torch.manualSeed(opt.seed)
+opt.savefile = cmd:string(opt.savefile, opt,
+    {save_every=true, print_every=true, savefile=true, vocabfile=true, datafile=true})
+    .. '.t7'
+
+local vocab_size = 4 -- 4 possible classes: double swing (impossible), single Stance L, single stance R, double stance
+
+
+protos = torch.load(opt.model)
+opt.rnn_size = protos.embed.weight:size(2)
+
+-- LSTM initial state (zero initially, but final state gets sent to initial state when we do BPTT)
+local initstate_c = torch.zeros(opt.batch_size, opt.rnn_size) -- initialize to zeros matrix
+local initstate_h = initstate_c:clone()
+
+-- do fwd
+
+    ------------------- forward pass -------------------
+    local embeddings = {}            -- input embeddings
+    local lstm_c = {[0]=initstate_c} -- internal cell states of LSTM
+    local lstm_h = {[0]=initstate_h} -- output values of LSTM
+    local predictions = {}           -- softmax outputs
+    local loss = 0
+
+    for t=1,opt.seq_length do
+        -- embeddings[t] = clones.embed[t]:forward(x[{{}, t}])
+        embeddings[t] = x[{{}, t}] -- assuming x is N x T tensor, with N being the marker data dimension (54) and T the time dimension
+
+        -- we're feeding the *correct* things in here, alternatively
+        -- we could sample from the previous timestep and embed that, but that's
+        -- more commonly done for LSTM encoder-decoder models
+        lstm_c[t], lstm_h[t] = unpack(protos.lstm:forward{embeddings[t], lstm_c[t-1], lstm_h[t-1]})
+
+        predictions[t] = protos.softmax:forward(lstm_h[t])
+		out[t]=predictions[t]:max();
+    end
+    loss = loss / opt.seq_length;
+	torch.save(opt.savefile, out)
