328492aaf6b5a6c2c9efea87e863020b3921872c
summivox <summivox@gmail.com>
2015-05-07 01:51:49 -0400

    complete overhaul of LSTM code

--- src/lstm/LSTM.lua
+++ src/lstm/LSTM.lua
@@ -4,31 +4,37 @@ local LSTM = {}
 
 -- Creates one timestep of one LSTM
 function LSTM.lstm(opt)
+    local input_size = opt.input_size
+    local rnn_size = opt.rnn_size
+
     local x = nn.Identity()()
     local prev_c = nn.Identity()()
     local prev_h = nn.Identity()()
 
-    function new_input_sum()--
-        -- transforms input
-        local i2h            = nn.Linear(opt.input_size,opt.rnn_size)(x) --Linear transformation of the input (i2h=Mi*x+bi). Weights are stored internally (?) 
-        -- transforms previous timestep's output
-        local h2h            = nn.Linear(opt.rnn_size, opt.rnn_size)(prev_h) --Linear transform, h2h=Mh*prev_h + bh
-        return nn.CAddTable()({i2h, h2h}) -- new_input = i2h + h2h;
+    -- create one {input/output/forget} gate
+    function new_gate()
+        -- linear transformation of the input : i2h=Mi*x+bi
+        local i2h = nn.Linear(input_size, rnn_size)(x)
+        -- linear transformation of output from previous timestep : h2h = Mh*prev_h + bh
+        local h2h = nn.Linear(rnn_size, rnn_size)(prev_h)
+        return nn.CAddTable()({i2h, h2h}) -- new_gate = i2h + h2h
     end
 
-    local in_gate          = nn.Sigmoid()(new_input_sum())
-    local forget_gate      = nn.Sigmoid()(new_input_sum())
-    local out_gate         = nn.Sigmoid()(new_input_sum())
-    local in_transform     = nn.Tanh()(new_input_sum()) 
+    local in_gate          = nn.Sigmoid()(new_gate())
+    local forget_gate      = nn.Sigmoid()(new_gate())
+    local out_gate         = nn.Sigmoid()(new_gate())
+    local in_transform     = nn.Tanh()(new_gate()) 
 
-    local next_c           = nn.CAddTable()({ -- c = forget_gate.*c + in_gate .* in_transform
+    -- c = forget_gate.*c + in_gate .* in_transform
+    local next_c = nn.CAddTable()({
         nn.CMulTable()({forget_gate, prev_c}),
         nn.CMulTable()({in_gate,     in_transform})
     })
-    local next_h           = nn.CMulTable()({out_gate, nn.Tanh()(next_c)}) -- CMultTable is element-wise multiplication (?). functionally this is h=tanh(c) .* out_gate
+
+    -- h=tanh(c) .* out_gate
+    local next_h           = nn.CMulTable()({nn.Tanh()(next_c), out_gate}) 
 
     return nn.gModule({x, prev_c, prev_h}, {next_c, next_h}) -- this defines a module with three inuputs: x,c,h and two outputs: c,h
 end
 
 return LSTM
-
---
+++ src/lstm/train2.lua
@@ -0,0 +1,146 @@
+require 'init_CPU'
+--require 'init_GPU'
+
+-- local BatchLoader = require 'data.BatchLoader'
+local BatchLoader = require 'BatchLoader'
+local LSTM = require 'LSTM'             -- LSTM timestep and utilities
+require 'Embedding'                     -- class name is Embedding (not namespaced)
+local model_utils=require 'model_utils'
+
+function isnan(x) return x ~= x end 
+
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text('Training a simple character-level LSTM language model')
+cmd:text()
+cmd:text('Options')
+cmd:option('-datafile','set1_1','filename of hdf5 data file')
+cmd:option('-batch_size',10,'size of minibatch')
+cmd:option('-seq_length',500,'number of timesteps to unroll to')
+cmd:option('-rnn_size',100,'size of LSTM internal state')
+cmd:option('-max_epochs',100,'number of full passes through the training data')
+cmd:option('-savefile','model_autosave','filename to autosave the model (net) to, appended with the epoch in which it was saved.t7')
+cmd:option('-save_every',1,'save every epoch') -- Changed to describe # of saving points as a function of epochs (instead of iterations)
+cmd:option('-print_every',1,'how many steps/minibatches between printing out the loss')
+cmd:option('-seed',20150507,'torch manual random number generator seed')
+cmd:option('-lr',1e-2,'learning rate')
+cmd:option('-lrd',1e-3,'learning rate decay for adagrad, in epochs: e.g .1 means it takes 10 epochs for the learning rate to decay to half its initial value, 20 epochs MORE to reach 1/4, 40 epochs MORE to 1/8 and so on')
+cmd:option('-clamp',20,'gradient clamp')
+cmd:text()
+opt = cmd:parse(arg)
+
+-- random seed
+torch.manualSeed(opt.seed)
+
+-- load data
+-- local loader = BatchLoader.create('../../data/' .. opt.datafile .. '.h5', opt.batch_size, opt.seq_length)
+local loader = BatchLoader.create(opt.datafile, opt.batch_size, opt.seq_length)
+local input_size = loader.input_size
+local output_size = loader.output_size
+opt.input_size = input_size
+opt.output_size = output_size
+
+-- build network
+-- Sigmoid() + BCECriterion() c.f. <http://qr.ae/0cUq0> (by Jack Rae, Google DeepMind)
+local net = {}
+net.lstm = LSTM.lstm(opt)
+net.output = nn.Sequential():add(nn.Linear(opt.rnn_size, output_size)):add(nn.Sigmoid())
+net.criterion = nn.BCECriterion()
+
+-- flatten parameters tensor
+local params, grad_params = model_utils.combine_all_parameters(net.lstm, net.output)
+params:uniform(-0.08, 0.08)
+
+-- LSTM initial state/output => zero
+local initstate_c = torch.zeros(opt.batch_size, opt.rnn_size)
+local initstate_h = initstate_c:clone()
+-- LSTM final state's backward message (dloss/dfinalstate) => zero
+local dfinalstate_c = initstate_c:clone()
+local dfinalstate_h = initstate_c:clone()
+
+-- optimization score function
+local current_batch = 1
+function feval(x)
+    if x ~= params then
+        params:copy(x)
+    end
+    grad_params:zero()
+
+    ------------------ get minibatch -------------------
+    -- dims: {time, batch, input/output_size}
+    local x, y = loader:next_batch()
+    current_batch = current_batch + 1
+
+    ------------------- forward pass -------------------
+    local lstm_c = {[0]=initstate_c} -- LSTM state
+    local lstm_h = {[0]=initstate_h} -- LSTM output
+    local output = {} -- network output
+    local loss = 0
+
+    for t=1,opt.seq_length do
+        xt = x:select(1, t)
+        yt = y:select(1, t)
+        lstm_c[t], lstm_h[t] = unpack(net.lstm:forward{xt, lstm_c[t-1], lstm_h[t-1]})
+        output[t] = net.output:forward(lstm_h[t])
+        newloss = net.criterion:forward(output[t], yt)
+        loss = loss + newloss
+        if isnan(loss) == true then
+            print(t, lstm_c[t-1]:size(), lstm_h[t-1]:size())
+            print(yt)
+            print(newloss)
+            error()
+        end
+    end
+
+    ------------------ backward pass -------------------
+    -- loss gradient w.r.t. :
+    local dlstm_h_last = dfinalstate_h -- LSTM output
+    local dlstm_c_last = dfinalstate_c -- LSTM state
+    for t=opt.seq_length,1,-1 do
+        xt = x:select(1, t)
+        yt = y:select(1, t)
+
+        -- backprop through criterion, output
+        local doutput_t = net.criterion:backward(output[t], yt)
+        local dlstm_h_t = dlstm_h_last + net.output:backward(lstm_h[t], doutput_t)
+
+        -- backprop through LSTM
+        _, dlstm_c_last, dlstm_h_last = unpack(net.lstm:backward(
+            {xt, lstm_c[t-1], lstm_h[t-1]},
+            {dlstm_c_last, dlstm_h_t}
+        ))
+    end
+
+    -- scale by minibatch size, then clamp
+    grad_params:div(opt.batch_size):clamp(-opt.clamp, opt.clamp)
+
+    return loss, grad_params
+end
+
+-- optimization
+torch.save(string.format(opt.savefile .. '_Params.t7', epoch) , opt)
+local optim_state = {learningRate = opt.lr, learningRateDecay = opt.lrd/loader.ns} -- For no decay set learningRateDecay=0, decay is implemented as inversely proportional to number of epoch evals in this way.
+local iterations = opt.max_epochs * loader.ns --loader.ns is the size of each epoch in iterations
+local losses = torch.zeros(iterations)
+for i = 1, iterations do -- one iteration is going through just 1 chunk of sequence, of length seq_length. If we have 30 sequences of 25secs each, with seq_length=100 (1s) it takes 25*30 =750 iterations to go through all the data once 
+	local epoch = i/loader.ns -- not an integer usually
+    local _, loss = optim.adagrad(feval, params, optim_state)
+    loss = loss[1]/opt.seq_length
+    losses[i] = loss
+
+    if i % (opt.save_every*loader.ns) == 0 then 
+        torch.save(string.format( opt.savefile .. '_Epoch%4d.t7', epoch), net)
+		torch.save(string.format( opt.savefile .. '_Loss.t7', epoch), opt)
+    end
+    if i % (opt.print_every*loader.ns) == 0 then
+        print(string.format("epoch %4d, loss = %6.8f, gradnorm = %6.4e", epoch, loss, grad_params:norm()))
+    end
+	if i == 1 then -- forcing initial print & save
+        print(string.format("epoch %4d, loss = %6.8f, gradnorm = %6.4e", epoch, loss, grad_params:norm()))
+		torch.save(string.format( opt.savefile .. '_Epoch%4d.t7', epoch), net)
+    end
+	if i == iterations then -- final save: get losses into matlab which is the easy thing to do
+		opt.loss = losses
+		mattorch.save(opt.savefile .. '_Results.mat', losses)
+	end
+end
